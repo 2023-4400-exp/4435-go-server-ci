47d3b30144bc7e4ca79067e4ac918b57e2504f90
m20125 <m20125@g.metro-cit.ac.jp>
2024-01-16 13:18:45 +0900

    add workflow

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "4444: ab ver.2")
+        return c.String(http.StatusOK, "4444: abc ver.2")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4444", "message": "ac"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4444", "message": "acc"})
     }
 }
